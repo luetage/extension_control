035169a28040963c6c7a781548e5a22313660a8d
luetage <luetage@zoho.com>
2018-04-17 13:15:26 +0200

    functions first

--- drop.js
+++ drop.js
@@ -1,6 +1,74 @@
 const switcher = document.getElementById('switcher');
 const thisID = chrome.runtime.id;
 
+function dark() {
+	document.body.style.setProperty('--bg', 'hsl(0,0%,10%)');
+	document.body.style.setProperty('--fg', 'hsl(207,5%,42%)');
+	document.body.style.setProperty('--fgShadow', 'hsl(207,5%,32%)');
+	document.body.style.setProperty('--headerBg', 'hsl(240,3%,13%)');
+	document.body.style.setProperty('--headerHi', 'hsl(228,57%,73%)');
+	document.body.style.setProperty('--headerHiShadow', 'hsl(228, 57%, 58%)');
+	document.body.style.setProperty('--headerFg', 'hsl(228,3%,66%)');
+	document.body.style.setProperty('--headerFgShadow', 'hsl(228,3%,51%)');
+	document.body.style.setProperty('--extBgHi', 'hsl(240,2%,19%)');
+	document.body.style.setProperty('--extFgHi', 'hsl(228,3%,66%)');
+	document.body.style.setProperty('--extFgHiShadow', 'hsl(228,3%,46%)');
+	document.body.style.setProperty('--extEn', 'hsl(228,57%,73%)');
+	document.body.style.setProperty('--extEnShadow', 'hsl(228,57%,53%)');
+	document.body.style.setProperty('--extEnHi', 'hsl(228,57%,73%)');
+	document.body.style.setProperty('--extEnHiShadow', 'hsl(228,57%,53%)');
+	document.body.style.setProperty('--menuBg', 'hsl(240,2%,19%)');
+	document.body.style.setProperty('--menuFgAct', 'hsl(210,2%,81%)');
+	document.body.style.setProperty('--menuFgActHi', 'hsl(210,2%,95%)');
+	document.body.style.setProperty('--optBgHi', 'hsl(170, 29%, 50%)');
+	document.body.style.setProperty('--uninstBgHi', 'hsl(349, 60%, 59%)');
+};
+
+function light() {
+	document.body.style.setProperty('--bg', 'hsl(0,0%,93%)');
+	document.body.style.setProperty('--fg', 'hsl(207,5%,58%)');
+	document.body.style.setProperty('--fgShadow', 'hsl(207,5%,73%)');
+	document.body.style.setProperty('--headerBg', 'hsl(240,3%,96%)');
+	document.body.style.setProperty('--headerHi', 'hsl(349, 60%, 59%)');
+	document.body.style.setProperty('--headerHiShadow', 'hsl(349, 60%, 74%)');
+	document.body.style.setProperty('--headerFg', 'hsl(228,3%,60%)');
+	document.body.style.setProperty('--headerFgShadow', 'hsl(228,3%,75%)');
+	document.body.style.setProperty('--extBgHi', 'hsl(240, 5%, 78%');
+	document.body.style.setProperty('--extFgHi', 'hsl(360, 100%, 100%)');
+	document.body.style.setProperty('--extEn', 'hsl(228,57%,36%)');
+	document.body.style.setProperty('--extEnShadow', 'hsl(228,57%,57%)');
+	document.body.style.setProperty('--extEnHi', 'hsl(228,57%,36%)');
+	document.body.style.setProperty('--extEnHiShadow', 'hsl(228,57%,57%)');
+	document.body.style.setProperty('--menuBg', 'hsl(240, 5%, 78%)');
+	document.body.style.setProperty('--menuFgAct', 'hsl(360, 100%, 100%)');
+	document.body.style.setProperty('--menuFgActHi', 'hsl(0, 0%, 94%)');
+	document.body.style.setProperty('--optBgHi', 'hsl(170, 29%, 50%)');
+	document.body.style.setProperty('--uninstBgHi', 'hsl(349, 60%, 59%)');
+};
+
+function gutter() {
+	var styleGut = document.createElement('style');
+	styleGut.innerHTML = '#header {padding: 16px 20px 11px;} .extension, #menu {padding: 5px 10px 5px 20px;} .extension.dev div::before {content:"$"} .extension.out div::before {content:"[]"}';
+	document.body.appendChild(styleGut);
+};
+
+function font() {
+	var styleWin = document.createElement('style');
+	styleWin.innerHTML = 'body {text-shadow: var(--fgShadow) 0 0 1px;} #header{text-shadow: var(--headerFgShadow) 0 0 1px;} #contrast{text-shadow: var(--headerHiShadow) 0 0 1px;} .extension:hover{text-shadow: var(--extFgHiShadow) 0 0 1px;} .extension.enabled {text-shadow: var(--extEnShadow) 0 0 1px;} .extension.enabled:hover {text-shadow: var(--extEnHiShadow) 0 0 1px;}';
+	document.body.appendChild(styleWin);
+};
+
+function rmMenu() {
+	var thisMenu = document.getElementById('menu');
+	if (thisMenu) {
+		thisMenu.outerHTML = '';
+	}
+};
+
+function options() {
+	chrome.runtime.openOptionsPage();
+};
+
 chrome.storage.sync.get({
 	'theme': '',
 	'width': '195',
@@ -113,74 +181,6 @@ chrome.management.getAll(function(info) {
   		}.bind(this, i));
 	}
 });
-
-function dark() {
-	document.body.style.setProperty('--bg', 'hsl(0,0%,10%)');
-	document.body.style.setProperty('--fg', 'hsl(207,5%,42%)');
-	document.body.style.setProperty('--fgShadow', 'hsl(207,5%,32%)');
-	document.body.style.setProperty('--headerBg', 'hsl(240,3%,13%)');
-	document.body.style.setProperty('--headerHi', 'hsl(228,57%,73%)');
-	document.body.style.setProperty('--headerHiShadow', 'hsl(228, 57%, 58%)');
-	document.body.style.setProperty('--headerFg', 'hsl(228,3%,66%)');
-	document.body.style.setProperty('--headerFgShadow', 'hsl(228,3%,51%)');
-	document.body.style.setProperty('--extBgHi', 'hsl(240,2%,19%)');
-	document.body.style.setProperty('--extFgHi', 'hsl(228,3%,66%)');
-	document.body.style.setProperty('--extFgHiShadow', 'hsl(228,3%,46%)');
-	document.body.style.setProperty('--extEn', 'hsl(228,57%,73%)');
-	document.body.style.setProperty('--extEnShadow', 'hsl(228,57%,53%)');
-	document.body.style.setProperty('--extEnHi', 'hsl(228,57%,73%)');
-	document.body.style.setProperty('--extEnHiShadow', 'hsl(228,57%,53%)');
-	document.body.style.setProperty('--menuBg', 'hsl(240,2%,19%)');
-	document.body.style.setProperty('--menuFgAct', 'hsl(210,2%,81%)');
-	document.body.style.setProperty('--menuFgActHi', 'hsl(210,2%,95%)');
-	document.body.style.setProperty('--optBgHi', 'hsl(170, 29%, 50%)');
-	document.body.style.setProperty('--uninstBgHi', 'hsl(349, 60%, 59%)');
-};
-
-function light() {
-	document.body.style.setProperty('--bg', 'hsl(0,0%,93%)');
-	document.body.style.setProperty('--fg', 'hsl(207,5%,58%)');
-	document.body.style.setProperty('--fgShadow', 'hsl(207,5%,73%)');
-	document.body.style.setProperty('--headerBg', 'hsl(240,3%,96%)');
-	document.body.style.setProperty('--headerHi', 'hsl(349, 60%, 59%)');
-	document.body.style.setProperty('--headerHiShadow', 'hsl(349, 60%, 74%)');
-	document.body.style.setProperty('--headerFg', 'hsl(228,3%,60%)');
-	document.body.style.setProperty('--headerFgShadow', 'hsl(228,3%,75%)');
-	document.body.style.setProperty('--extBgHi', 'hsl(240, 5%, 78%');
-	document.body.style.setProperty('--extFgHi', 'hsl(360, 100%, 100%)');
-	document.body.style.setProperty('--extEn', 'hsl(228,57%,36%)');
-	document.body.style.setProperty('--extEnShadow', 'hsl(228,57%,57%)');
-	document.body.style.setProperty('--extEnHi', 'hsl(228,57%,36%)');
-	document.body.style.setProperty('--extEnHiShadow', 'hsl(228,57%,57%)');
-	document.body.style.setProperty('--menuBg', 'hsl(240, 5%, 78%)');
-	document.body.style.setProperty('--menuFgAct', 'hsl(360, 100%, 100%)');
-	document.body.style.setProperty('--menuFgActHi', 'hsl(0, 0%, 94%)');
-	document.body.style.setProperty('--optBgHi', 'hsl(170, 29%, 50%)');
-	document.body.style.setProperty('--uninstBgHi', 'hsl(349, 60%, 59%)');
-};
-
-function gutter() {
-	var styleGut = document.createElement('style');
-	styleGut.innerHTML = '#header {padding: 16px 20px 11px;} .extension, #menu {padding: 5px 10px 5px 20px;} .extension.dev div::before {content:"$"} .extension.out div::before {content:"[]"}';
-	document.body.appendChild(styleGut);
-};
-
-function font() {
-	var styleWin = document.createElement('style');
-	styleWin.innerHTML = 'body {text-shadow: var(--fgShadow) 0 0 1px;} #header{text-shadow: var(--headerFgShadow) 0 0 1px;} #contrast{text-shadow: var(--headerHiShadow) 0 0 1px;} .extension:hover{text-shadow: var(--extFgHiShadow) 0 0 1px;} .extension.enabled {text-shadow: var(--extEnShadow) 0 0 1px;} .extension.enabled:hover {text-shadow: var(--extEnHiShadow) 0 0 1px;}';
-	document.body.appendChild(styleWin);
-};
-
-function rmMenu() {
-	var thisMenu = document.getElementById('menu');
-	if (thisMenu) {
-		thisMenu.outerHTML = '';
-	}
-};
-
-function options() {
-	chrome.runtime.openOptionsPage();
-};
 
 switcher.addEventListener('mouseleave', rmMenu);
 switcher.addEventListener('click', rmMenu);
